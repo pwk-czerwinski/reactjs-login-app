7e9e1ead88bad15b7a68ca5703e400352715c609
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2021-04-16 21:00:30 +0200

    Prepared improvement of the login form by using Material UI (1)

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,6 +1,33 @@
-import React, { useState } from "react";
+import React, {ChangeEvent, useState} from 'react';
+import {
+  Avatar, Button, CssBaseline, TextField, Typography, Container
+} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const LoginForm = () => {
+  const classes = useStyles();
+
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
@@ -33,27 +60,55 @@ const LoginForm = () => {
   };
 
   return (
-    <form className="form" onSubmit={submit}>
-      <div>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <button type="submit">Sign in</button>
-      </div>
-    </form>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} onSubmit={submit}>
+            <TextField
+                variant="outlined"
+                margin="normal"
+                id="email"
+                label="Username"
+                placeholder="Username"
+                name="email"
+                type="text"
+                value={username}
+                onChange={handleUsername}
+                autoFocus
+                required
+                fullWidth
+            />
+            <TextField
+                variant="outlined"
+                margin="normal"
+                id="password"
+                label="Password"
+                placeholder="Password"
+                name="password"
+                type="password"
+                value={password}
+                onChange={handlePassword}
+                required
+                fullWidth
+            />
+            <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                fullWidth
+            >
+              Sign In
+            </Button>
+          </form>
+        </div>
+      </Container>
   );
 };
 
