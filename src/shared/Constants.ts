6bb455ec62cc96b11a314fcddd02c78e2fe1afba
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2021-04-21 22:04:08 +0200

    Created constants used in the whole application

---
+++ src/shared/Constants.ts
@@ -0,0 +1,9 @@
+/**
+ * Here are located all constants used in the application.
+ */
+class Constants {
+    public static readonly TOKEN_COOKIE_NAME = 'token';
+    public static readonly TOKEN_COOKIE_EXPIRATION_DAYS = 20;
+}
+
+export default Constants;
