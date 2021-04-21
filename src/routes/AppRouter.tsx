72b8c6c7a4e2e64e4a753a3bf12f99bfd2b273c7
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2021-04-21 21:52:29 +0200

    Added private /dashboard route

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Redirect } from 'react-router-dom';
 import Routes from './Routes';
 import PublicRoute from './PublicRoute';
+import PrivateRoute from './PrivateRoute';
 import LoginForm from '../components/login/LoginForm';
+import Dashboard from '../components/dashboard/Dashboard';
 
 /**
  * Router with all routes in the whole application.
@@ -11,6 +13,7 @@ const AppRouter = () => (
     <BrowserRouter>
         <Switch>
             <PublicRoute restricted component={LoginForm} path={Routes.Login} exact />
+            <PrivateRoute component={Dashboard} path={Routes.Dashboard} exact />
             <Redirect to={Routes.Login} />
         </Switch>
     </BrowserRouter>
