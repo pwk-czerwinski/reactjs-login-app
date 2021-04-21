297f9ec1f5065ed3e612ada8b6b36baec84b8f46
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2021-04-22 00:47:22 +0200

    Prepared tests branch with axios mock

--- src/__mocks__/axios.ts
+++ src/__mocks__/axios.ts
@@ -1,10 +1,8 @@
-// const mockAxios = jest.genMockFromModule('axios');
-// mockAxios.create = jest.fn(() => mockAxios);
+const mockAxios = jest.genMockFromModule('axios');
+mockAxios.create = jest.fn(() => mockAxios);
 
 // const mockAxios = jest.mock('axios', () => ({
 //     create: jest.fn(() => mockAxios)
 // }));
 
-// export default mockAxios;
-
-export {};
+export default mockAxios;
