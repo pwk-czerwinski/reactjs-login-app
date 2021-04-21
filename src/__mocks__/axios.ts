bcdc9a46fc93d3a2bc31ffee6279e9b4896b88cf
pwk-czerwinski <pwkczerwinski.work@gmail.com>
2021-04-22 00:53:00 +0200

    Commented axios mock which produces some errors...

--- src/__mocks__/axios.ts
+++ src/__mocks__/axios.ts
@@ -1,8 +1,10 @@
-const mockAxios = jest.genMockFromModule('axios');
-mockAxios.create = jest.fn(() => mockAxios);
+// const mockAxios = jest.genMockFromModule('axios');
+// mockAxios.create = jest.fn(() => mockAxios);
 
 // const mockAxios = jest.mock('axios', () => ({
 //     create: jest.fn(() => mockAxios)
 // }));
 
-export default mockAxios;
+// export default mockAxios;
+
+export default {};
